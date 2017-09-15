9916ad121b5447cc64df621ce38a4d634ff2b4e7
FruitBoss <chserg81@yandex.ru>
2017-09-15 19:52:30 +0500

    test

--- 1.php
+++ 1.php
@@ -6,9 +6,6 @@
         <script src="js/jquery-3.2.1.min.js"></script>
     </head>
     <body>
-        <?php
-        $one = md5('153403'.'2017');
-        echo $one;
-        ?>
+
     </body>
 </html>
